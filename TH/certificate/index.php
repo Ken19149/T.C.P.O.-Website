926312295eb988d2029eebfc7a588184aee97a60
Ken980 <kenboy9800@gmail.com>
2020-11-17 09:21:03 +0700

    add thai language folder

---
+++ TH/certificate/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>T.C.P.O.</title>
+        <link rel="shortcut icon" href="">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril Fatface">
+    </head>
+    <body>
+    <div id="div">
+        <div id="banner">
+            <b><h1><a href="">&nbsp;&nbsp;TCPO<span id="co-th">.co.th</span></a></h1></b>
+        </div>
+        <div id="nav">
+            <center><h1>
+                <a href="../"><i class="fa fa-fw fa-home"></i>Home</a><a href="../gallery"><i class="fa fa-image"></i> Gallery</a><a href="../about"><i class="fa fa-info-circle"></i> About</a><a href=""><i class="fa fa-certificate"></i> Certificate</a><a href="../contact"><i class="fa fa-address-card"></i> Contact</a>
+            </h1></center>
+        </div>
+    </div>
+    </body>
+</html>
